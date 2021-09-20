f315dbe263ee34bfab5d9e8be2a427d1be2bfeb2
Sedat Başpınar <sdtbsp10@gmail.com>
2021-09-21 02:55:28 +0300

    user model created

---
+++ src/main/java/com/sedatbsp/bookstore/model/Role.java
@@ -0,0 +1,12 @@
+package com.sedatbsp.bookstore.model;
+
+/**
+ * @author Sedat Başpınar
+ * @created 21.09.2021 - 2:48 AM
+ * @project book-store
+ */
+public enum Role {
+    USER, // default
+    ADMIN, // crud
+    SYSTEM_ADMIN // internal
+}
---
+++ src/main/java/com/sedatbsp/bookstore/model/User.java
@@ -0,0 +1,38 @@
+package com.sedatbsp.bookstore.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * @author Sedat Başpınar
+ * @created 21.09.2021 - 2:35 AM
+ * @project book-store
+ */
+@Data
+@Entity
+@Table(name = "users") // Default olarak user kullanamayız, çünkü postgresql'de user ayrılmış bir ad'dır.
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username",unique = true,nullable = false)
+    private String username;
+
+    @Column(name = "password",nullable = false)
+    private String password;
+
+    @Column(name = "name",nullable = false)
+    private String name;
+
+    @Column(name="create_time",nullable = false)
+    private LocalDateTime createTime;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role",nullable = false)
+    private Role role;
+
+}
