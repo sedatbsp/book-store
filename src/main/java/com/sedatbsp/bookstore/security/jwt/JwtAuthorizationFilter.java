c2a0267f0ca4198ff26f82aa4ac81363d29f91fc
Sedat Başpınar <sdtbsp10@gmail.com>
2021-09-25 13:35:47 +0300

    JWT Authentication filter created

---
+++ src/main/java/com/sedatbsp/bookstore/security/jwt/JwtAuthorizationFilter.java
@@ -0,0 +1,35 @@
+package com.sedatbsp.bookstore.security.jwt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Sedat Başpınar
+ * @created 25.09.2021 - 1:20 PM
+ * @project book-store
+ */
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private IJwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        Authentication authentication = jwtProvider.getAuthentication(request);
+
+        if(authentication != null && jwtProvider.validateToken(request)){
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request,response);
+
+    }
+}
