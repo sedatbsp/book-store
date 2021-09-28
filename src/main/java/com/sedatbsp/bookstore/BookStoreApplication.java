f071d2fa6b22327d1ee277184f5c97867bcd7122
Sedat Başpınar <sdtbsp10@gmail.com>
2021-09-28 19:52:56 +0300

    heroku configuration

--- src/main/java/com/sedatbsp/bookstore/BookStoreApplication.java
+++ src/main/java/com/sedatbsp/bookstore/BookStoreApplication.java
@@ -3,10 +3,12 @@ package com.sedatbsp.bookstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class BookStoreApplication {
 
 	public static void main(String[] args) {
